4f73370a08bf495c64c47a059b811ae7af6c39f1
Angeliss <59443350+lion-goose@users.noreply.github.com>
2020-11-27 10:51:17 +0800

    Update smartReplace.js

--- smartReplace.js
+++ smartReplace.js
@@ -22,9 +22,9 @@ async function replaceWithSecrets(content, Secrets) {
         if (Secrets.COOKIE_ELM) {
             replacements.push({ key: "sy.getdata(cookieKey)", value: JSON.stringify(Secrets.COOKIE_ELM) });
         }
-        if (Secrets.JD_COOKIE && content.indexOf("京东赚赚") > 0) {
-            replacements.push({ key: ', $.getdata("jdzz_token2") || "";', value: "" });
-            replacements.push({ key: '$.getdata("jdzz_token1")', value: JSON.stringify(Secrets.JD_TOKEN) });
+        if (Secrets.JD_COOKIE && content.indexOf("京东赚赚") > 0) {           
+            replacements.push({ key: '$.getdata("jdzz_token1")', value: JSON.stringify(Secrets.JD_TOKEN.split("\n")[0])) });
+            replacements.push({ key: '$.getdata("jdzz_token2")', value: JSON.stringify(Secrets.JD_TOKEN.split("\n")[1]) });
         }
         if (Secrets.COOKIE_QQYD) {
             replacements.push({ key: "$.getdata(qqreadurlKey)", value: JSON.stringify(Secrets.COOKIE_QQYD.split("\n")[0]) });
