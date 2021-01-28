476baf4ec1b8923f165bc0492e3a491935ea0920
Angeliss <59443350+lion-goose@users.noreply.github.com>
2021-01-29 00:20:48 +0800

    Update crontab_list.sh

--- Scripts/crontab_list.sh
+++ Scripts/crontab_list.sh
@@ -20,21 +20,6 @@
 # # 工业品爱消除
 # 20 * * * * node /scripts/jd_gyec.js >> /scripts/logs/jd_gyec.log 2>&1
 
-# 超级直播间红包雨
-30,31 20-23/1 25 1 * node /scripts/jd_live_redrain.js >> /scripts/logs/jd_live_redrain.log 2>&1
-
-# 直播间红包雨
-0,1 19-21/1 * * * node /scripts/jd_live_redrain2.js >> /scripts/logs/jd_live_redrain2.log 2>&1
-
-# 半点红包雨
-30,31 12-23/1 * * * node /scripts/jd_live_redrain_half.js >> /scripts/logs/jd_live_redrain_half.log 2>&1
-
-# 年货直播红包雨
-0 0,9,11,13,15,17,19,20,21,23 3,5,20-30/1 1,2 * node /scripts/jd_live_redrain_nian.js >> /scripts/logs/jd_live_redrain_nian.log 2>&1
-
-# 官方号直播红包雨
-0 0,9,11,13,15,17,19,20,21,22,23 * * * node /scripts/jd_live_redrain_offical.js >> /scripts/logs/jd_live_redrain_offical.log 2>&1
-
 # 神券驾到
 1 7 13 1 * node /scripts/jd_super_coupon.js >> /scripts/logs/jd_super_coupon.log 2>&1
 
