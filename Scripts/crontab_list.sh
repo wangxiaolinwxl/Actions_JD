e5e23c24ccb1db60ccc64512c045d18c78922345
Angeliss <59443350+lion-goose@users.noreply.github.com>
2021-02-24 17:39:14 +0800

    Update crontab_list.sh

--- Scripts/crontab_list.sh
+++ Scripts/crontab_list.sh
@@ -12,7 +12,7 @@
 # 美丽研究院
 51 6,13,18 * * * node /scripts/jd_beauty.js >> /scripts/logs/jd_beauty.log 2>&1
 # 签到领现金
-10 7 * * * node /scripts/jd_cash.js >> /scripts/logs/jd_cash.log 2>&1
+32 7 * * * node /scripts/jd_cash.js >> /scripts/logs/jd_cash.log 2>&1
 # 导出所有互助码
 47 5 */2 * * node /scripts/jd_get_share_code.js >> /scripts/logs/jd_get_share_code.log 2>&1
 
